0af9483769e4dd8096f58de0c4647c8cf7afe475
Opderbecado <diegonicolasvespa@gmail.com>
2017-03-16 11:58:54 -0300

    asd

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+import datetime
+class alumno:
+    nombre=" "
+    apellido= " "
+    fecha_nac = None
+    lista_notas = []
+    def setNombre (self, n):
+        self.nombre=str(n)
+
+    def setApellido (self, a):
+        self.apellido = str(a)
+
+    def setFecha (self):
